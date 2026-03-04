23f530365956328535564847c78043af880db8d0
Grld-Andy <andyansong82@gmail.com>
2026-03-04 12:35:13 +0000

    style: style dashboard title cards

--- client/src/components/custom/Dashboard/StatsCard.tsx
+++ client/src/components/custom/Dashboard/StatsCard.tsx
@@ -1,19 +1,43 @@
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import React from "react";
+import type { DashboardTitleCards } from "@/types/DashboardTitleCards";
 
-const StatsCard: React.FC = () => {
+interface Props {
+  cardDetails: DashboardTitleCards;
+}
+
+const StatsCard: React.FC<Props> = ({ cardDetails }) => {
   return (
-    <Card className="w-full max-w-sm">
-      <CardHeader>
-        <CardTitle>Open Incidents</CardTitle>
-      </CardHeader>
-      <CardContent>
-        24
-      </CardContent>
-      <CardFooter className="flex-col gap-2">
-        +25% Might add percentage
-      </CardFooter>
-    </Card>
+    <div className="flex gap-2 bg-white flex-col shadow-md border-[1px] border-black/20 rounded-2xl p-3">
+      <div className="flex flex-col justify-between h-full">
+        <div>
+          <p className="text-[1.2em] font-[400]">{cardDetails.title}</p>
+        </div>
+        <div>
+          <p className="font-bold text-[3em]">{cardDetails.count}</p>
+        </div>
+      </div>
+      <div className="flex-col gap-2">
+        <div className={`flex gap-1 text-[0.8em]
+              ${cardDetails.color == "green" ? "text-green-900" : ""}
+              ${cardDetails.color == "red" ? "text-red-900" : ""}
+              ${cardDetails.color == "orange" ? "text-orange-900" : ""}
+              ${cardDetails.color == "blue" ? " text-blue-900" : ""}
+              ${cardDetails.color == "purple" ? "text-purple-900" : ""}`
+            }>
+          <span className={`
+              grid place-items-center w-[70px] h-[20px] border-[1px]  rounded-full
+              ${cardDetails.color == "green" ? "bg-green-300 border-green-600" : ""}
+              ${cardDetails.color == "red" ? "bg-red-300 border-red-600" : ""}
+              ${cardDetails.color == "orange" ? "bg-orange-300 border-orange-600" : ""}
+              ${cardDetails.color == "blue" ? "bg-blue-300 border-blue-600" : ""}
+              ${cardDetails.color == "purple" ? "bg-purple-300 border-purple-600" : ""}
+            `}>
+            {cardDetails.percentage}%
+          </span>
+          <p className="text-nowrap">Might add percentage</p>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- client/src/pages/ProtectedPages/Dashboard.tsx
+++ client/src/pages/ProtectedPages/Dashboard.tsx
@@ -1,9 +1,18 @@
 import DashboardNavbar from "@/components/custom/Dashboard/DashboardNavbar";
 import StatsCard from "@/components/custom/Dashboard/StatsCard";
 import { Button } from "@/components/ui/button";
+import type { DashboardTitleCards } from "@/types/DashboardTitleCards";
 import React from "react";
 
 const Dashboard: React.FC = () => {
+  const dashboardCards : Array<DashboardTitleCards> = [
+    {title: "Days Since Last Incident", count: 1, percentage: 10, color: "blue", isIncrese: true},
+    {title: "Open Incidents", count: 25, percentage: 20, color: "green", isIncrese: true},
+    {title: "Critical Incidents", count: 5, percentage: 2, color: "red", isIncrese: false},
+    {title: "Closed Incidents", count: 25, percentage: 20, color: "purple", isIncrese: false},
+    {title: "Resolved Today", count: 25, percentage: 20, color: "orange", isIncrese: true},
+  ]
+
   return (
     <div className="flex flex-col gap-5 w-full">
       <DashboardNavbar />
@@ -22,10 +31,10 @@ const Dashboard: React.FC = () => {
       </div>
 
       {/* stats cards */}
-        <div className="flex gap-3 overflow-x-scroll hide-scrollbar">
+        <div className="flex gap-3 overflow-x-scroll hide-scrollbar py-3 px-1">
           {
-            Array.from({ length: 4 }).map((_, index) => (
-              <StatsCard key={index}/>
+            dashboardCards.map((_, index) => (
+              <StatsCard cardDetails={dashboardCards[index]} key={index}/>
             ))
           }
         </div>
