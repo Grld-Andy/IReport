36636f02226d9340bf395f742d4c81e532d0ba43
Grld-Andy <andyansong82@gmail.com>
2026-04-01 10:41:18 +0000

    feat: add files

---
+++ server/src/Modules/Organization/SafeZone.Modules.Organization.Api/OrganizationModule.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SafeZone.Shared.Abstractions.Modules;
+
+namespace SafeZone.Modules.Organization.Api;
+
+internal sealed class OrganizationModule : IModule
+{
+    public string Name { get; } = "Organization";
+        
+    public IEnumerable<string> Policies { get; } = new[]
+    {
+        "organization"
+    };
+
+    public void Register(IServiceCollection services, IConfiguration configuration)
+    {
+        // Optional: register core services here
+    }
+        
+    public void Use(IApplicationBuilder app)
+    {
+        // Optional: add middleware here
+    }
+
+    public void Expose(IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapGet("/organization/status", () =>
+        {
+            return Results.Ok("Organization is working");
+        })
+        .WithTags("Organization")
+        .WithName("Get Organization Status");
+    }
+}
---
+++ server/src/Modules/Payment/SafeZone.Modules.Payment.Api/PaymentModule.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SafeZone.Shared.Abstractions.Modules;
+
+namespace SafeZone.Modules.Payment.Api;
+
+internal sealed class PaymentModule : IModule
+{
+    public string Name { get; } = "Payment";
+        
+    public IEnumerable<string> Policies { get; } = new[]
+    {
+        "payment"
+    };
+
+    public void Register(IServiceCollection services, IConfiguration configuration)
+    {
+        // Optional: register core services here
+    }
+        
+    public void Use(IApplicationBuilder app)
+    {
+        // Optional: add middleware here
+    }
+
+    public void Expose(IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapGet("/payment/status", () =>
+        {
+            return Results.Ok("Payment is working");
+        })
+        .WithTags("Payment")
+        .WithName("Get Payment Status");
+    }
+}
